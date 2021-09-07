eaca76254583a3cd18257858c86f0deacaa82f9b
skogard <skogard@protonmail.com>
2021-09-07 10:43:34 -0400

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Rariverse
+
+> Deploy your own Decentralized NFT Marketplace Contract with One Click
+
+![rariverse.png](rariverse.png)
+
+rariverse is a static HTML website that lets you deploy the [Rarible Decentralized NFT Marketplace Contract](https://rarible.org) to Ethereum with ONE CLICK.
+
+You can reach the website at:
+
+- **Over HTTP:** https://rariverse.org
+- **Over IPFS:** https://ipfs.io/ipfs/bafkreiaungh2lljwaz5v4yaxqk4eypb2x5ebhjfxqh4gtys7a7jmcv5sie
+
+> Note: Try deploying on testnet first.
---
+++ tokenize.js
@@ -0,0 +1,37 @@
+const Rarepress = require('rarepress')
+const fs = require('fs')
+const rarepress = new Rarepress();
+(async () => {
+  await rarepress.init({ network: "mainnet" })
+  let codeBuffer = await fs.promises.readFile("index.html")
+  let imageBuffer = await fs.promises.readFile("rariverse.png")
+  let code_cid = await rarepress.fs.add(codeBuffer)
+  let image_cid = await rarepress.fs.add(imageBuffer)
+  let token = await rarepress.token.create({
+    type: "ERC1155",
+    supply: 7,
+    metadata: {
+      name: "Rariverse",
+      description: "A 7 of 7 ERC1155 NFT that tokenizes the FIRST snapshot of Rariverse, a decentralized website that lets you deploy a decentralized NFT marketplace contract to Ethereum.",
+      image: "/ipfs/" + image_cid,
+      attributes: [{
+        trait_type: "HTML over IPFS Gateway",
+        value: "https://ipfs.io/ipfs/" + code_cid
+      }, {
+        trait_type: "website hosted on IPFS",
+        value: code_cid
+      }, {
+        trait_type: "GitHub",
+        value: "https://github.com/rarepress/rariverse"
+      }, {
+        trait_type: "HTTP Website",
+        value: "https://rariverse.org"
+      }]
+    }
+  })
+  await rarepress.fs.push(code_cid)
+  await rarepress.fs.push(image_cid)
+  await rarepress.fs.push(token.uri)
+  let sent = await rarepress.token.send(token)
+  console.log("sent", sent)
+})();
